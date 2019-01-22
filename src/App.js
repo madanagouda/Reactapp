b99432cda597c2a047be7e03fe1309d2f39f226e
madanagouda <madanagouda@gmail.com>
2019-01-22 15:58:16 +0530

    latest commit with hello name message

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import Personalnfo from './personal/Personal';
 import Education from './personal/Education';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.handleChange=this.handleChange.bind(this);
+    this.state = {
+            name:"",
+            status:false
+    }   
+
+  }
+  handleChange(event){
+   this.setState({name:event.target.value,status:true});
+   
+  }
   render() {
     return (
       <div className="App">
@@ -23,10 +36,11 @@ class App extends Component {
           >
            
           </a>
-          <Education/>
+          <p>Your name please</p>
+            <input type="text" name="name" placeholder={"Your Name"} onBlur={this.handleChange.bind(this)} />  
+          <Education  name={this.state.name} status={this.state.status}/>
         </header>
-        
-          <h2>Madanagouda Goudar</h2>
+        <h2>Madanagouda Goudar</h2>
         
       </div>
     );
--- src/personal/Education.js
+++ src/personal/Education.js
@@ -1,13 +1,21 @@
 import React from "react";
 
-function Education(){
-    return (
-        <div>
-                <p>Play Here</p>
-                <input type={"text"} name={"name"} placeholder={"Your Name"} onChange={this.displayit()}/>  
-        </div>
-        
-    );
+function Education(props){
+    console.log(props.name);
+    if(props.status===true){
+        return (
+            <div>
+                <h3>Hi {props.name}. How are You.</h3>
+            </div>
+        );
+    }else{
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+    
 }
 
 export default Education;
